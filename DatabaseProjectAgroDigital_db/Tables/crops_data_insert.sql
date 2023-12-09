640220ae65c72b3802e909430736606f49fdaebe
Rayan Sikandar <ryansikandar@gmail.com>
2023-12-09 23:46:12 +0500

    added data for crops

---
+++ DatabaseProjectAgroDigital_db/Tables/crops_data_insert.sql
@@ -0,0 +1,14 @@
+-- INSERT into Crops
+-- VALUES ('Wheat','Summer'),('Rice','Winter'),('Sugarcane','Summer');
+
+-- Insert data into the Crop_Conditions table for three crops
+-- INSERT INTO Crop_Conditions (crop_id, temp_low, temp_high, plant_start, plant_end, harvest_start, harvest_end) VALUES
+-- (100, 20.0, 30.0, '2023-03-01', '2023-04-01', '2023-06-01', '2023-07-01'),
+-- (101, 25.0, 35.0, '2023-04-01', '2023-05-01', '2023-07-01', '2023-08-01'),
+-- (102, 18.0, 28.0, '2023-05-01', '2023-06-01', '2023-08-01', '2023-09-01');
+
+-- Insert data for three entries with farm IDs starting from 53000 and crop IDs starting from 100
+-- INSERT INTO farm_crops (crop_id, farm_id, planting_date, harvest_prediction) VALUES
+-- (100, 53000, '2023-03-01', '2023-06-01'),
+-- (101, 53001, '2023-03-15', '2023-06-15'),
+-- (102, 53002, '2023-04-01', '2023-07-01');
---
+++ DatabaseProjectAgroDigital_db/Tables/markets_data_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO Markets
+VALUES (10000,'Gujranwala Market'),(10001,'Sheikhupura Market'),(10002,'Lahore Market');
+
+INSERT INTO Crop_Prices
+VALUES(13000,);
