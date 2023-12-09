08ca2d010d81270178ff38b5b2b96feaa7cf71af
Rayan Sikandar <ryansikandar@gmail.com>
2023-12-09 23:59:05 +0500

    added some data

--- DatabaseProjectAgroDigital_db/Tables/forums.sql
+++ DatabaseProjectAgroDigital_db/Tables/forums.sql
@@ -1,15 +1,15 @@
-CREATE TABLE Topic
-(
-  topic_id INT PRIMARY KEY IDENTITY(31000, 1),
-  topic_name VARCHAR(40),
-  total_messages INT,
-  date_created DATETIME
-)
+-- CREATE TABLE Topic
+-- (
+--   topic_id INT PRIMARY KEY IDENTITY(31000, 1),
+--   topic_name VARCHAR(40),
+--   total_messages INT,
+--   date_created DATETIME
+-- )
 
-CREATE TABLE Discussions
-(
-  discussion_id INT PRIMARY KEY IDENTITY(35000,1),
-  user_id INT FOREIGN KEY REFERENCES Users(user_id),
-  content VARCHAR(2000),
-  topic_id INT FOREIGN KEY REFERENCES Topic(topic_id),
-)
+-- CREATE TABLE Discussions
+-- (
+--   discussion_id INT PRIMARY KEY IDENTITY(35000,1),
+--   user_id INT FOREIGN KEY REFERENCES Users(user_id),
+--   content VARCHAR(2000),
+--   topic_id INT FOREIGN KEY REFERENCES Topic(topic_id),
+-- )
