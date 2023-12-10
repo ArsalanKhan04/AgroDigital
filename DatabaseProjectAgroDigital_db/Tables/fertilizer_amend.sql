ec7bd5817d815ca2501772d23f8d15c8bc38df9a
Rayan Sikandar <ryansikandar@gmail.com>
2023-12-10 16:45:34 +0500

    new branch for frontend

--- DatabaseProjectAgroDigital_db/Tables/fertilizer_amend.sql
+++
@@ -1,8 +0,0 @@
--- Write your own SQL object definition here, and it'll be included in your package.
-
-ALTER TABLE Fertilizer_Company 
-ADD contact_no VARCHAR(25);
-
-ALTER TABLE Fertilizer_Company
-ADD email VARCHAR(25);
-
--- DatabaseProjectAgroDigital_db/Tables/markets_data_insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Markets
-VALUES (10000,'Gujranwala Market'),(10001,'Sheikhupura Market'),(10002,'Lahore Market');
-
