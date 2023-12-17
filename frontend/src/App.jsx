ff7431a26d40068b87e4a1ba297c193355b1d571
Rayan Sikandar <ryansikandar@gmail.com>
2023-12-17 14:37:36 +0500

    routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './Pages/Home';
-import Farm from './Pages/Farm';
-import Search from './Pages/Search';
-import CarouselComponent from './Pages/Home';
-import Forum from './Pages/forum';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import Farm from "./Pages/Farm";
+import Search from "./Pages/Search";
+import CarouselComponent from "./Pages/Home";
+import Forum from "./Pages/forum";
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path='/' element={Navbar} />
-    //   </Routes>
-    // </Router>
-    // <Farm/>
-    // <Home/>
-    <Forum/>
-
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route element={<Farm/>} path="/farm"/>
+      <Route element={<Search />} path="/search"/>
+      <Route element={<Forum/>} path="/forum"/>
+      <Route element={<Home/>} path="/"/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import '../styles/navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav>
         <div className="logo">
@@ -8,11 +10,11 @@ const Navbar = () => {
             <h2>AgroDigital</h2>
         </div>
         <div className="links">
-            <h5 className="links">Farms</h5>
+            <h5 className="links" onClick={() => navigate("/farm", { replace: true })}>Farms</h5>
             <h5 className="links">Information</h5>
             <h5 className="links">Market</h5>
-            <h5 className="links">Forums Page</h5>
-            <h5 className="links">My Profile</h5>
+            <h5 className="links" onClick={() => navigate("/forum",{replace:true})}>Forums Page</h5>
+            <h5 className="links" onClick={() => navigate("/search",{replace:true})}>Search</h5>
         </div>
     </nav>
   )
