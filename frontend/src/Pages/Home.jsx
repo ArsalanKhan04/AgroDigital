f57b4879ca5ab62070ac2be2c16c252e853d179b
Rayan Sikandar <ryansikandar@gmail.com>
2023-12-18 15:36:14 +0500

    completed map section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 // CarouselComponent.js
-import React from "react";
+import { useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Navbar from "../components/Navbar";
@@ -13,8 +13,45 @@ import {
   DialogTrigger,
 } from "../Components/ui/dialogue";
 import { useNavigate } from "react-router-dom";
+import Map from "./Map";
 
 const Home = () => {
+  const [farmData, setFarmData] = useState({
+    farmName: "",
+    cityName: "",
+    district: "",
+    area: 0,
+    location: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFarmData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    // Add your code to send data to the backend using a POST API
+    const longitude = localStorage.getItem("lng");
+    const latitude = localStorage.getItem("lat");
+    fetch("/api/farm", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...farmData, longitude, latitude }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data sent successfully:", data);
+        // Optionally, you can navigate to the map or handle success as needed
+        navigate("/map", { replace: true });
+      })
+      .catch((error) => {
+        console.error("Error sending data:", error);
+        // Handle error as needed
+      });
+  };
+
   const navigate = useNavigate();
   return (
     <div id="root">
@@ -40,14 +77,36 @@ const Home = () => {
                   <div>
                     <label>Enter the name of the Farm: </label>
                     <input
+                      onChange={handleInputChange}
                       type="text"
                       placeholder="Name of the farm"
                       className="p-4 pl-10 pr-10 rounded-md"
                     />
                   </div>
                   <div>
+                    <label>Enter City Name: </label>
+                    <input
+                      onChange={handleInputChange}
+                      type="text"
+                      placeholder="Name of the City"
+                      className="p-4 pl-10 pr-10 rounded-md"
+                    />
+                  </div>
+                  <div>
+                    <label>Select District: </label>
+                    <select
+                      className="p-4 pl-10 pr-10 rounded-md"
+                      onChange={handleInputChange}
+                    >
+                      <option value="type1">Lahore</option>
+                      <option value="type2">Sargodha</option>
+                      {/* Add more options as needed */}
+                    </select>
+                  </div>
+                  <div>
                     <label>Enter the area of the Farm: </label>
                     <input
+                      onChange={handleInputChange}
                       type="number"
                       placeholder="Area of the farm"
                       className="p-4 pl-10 pr-10 rounded-md"
@@ -56,19 +115,30 @@ const Home = () => {
                   <div>
                     <label>Enter the Location of the Farm: </label>
                     <input
+                      onChange={handleInputChange}
                       type="text"
                       placeholder="Location of the farm"
                       className="p-4 pl-10 pr-10 rounded-md"
                     />
                   </div>
-                  <div className="flex justify-center">
-                    <button
-                      onClick={() => navigate("/map", { replace: true })}
-                      className="py-2 px-4 bg-white"
-                    >
-                      Open Map
-                    </button>
-                  </div>
+
+                  <Dialog>
+                    <DialogTrigger>
+                      <div className="flex justify-center">
+                        <button
+                          className="py-2 px-4 bg-white"
+                        >
+                          Open Map
+                        </button>
+                      </div>
+                    </DialogTrigger>
+                    <DialogContent className="bg-[#5caf74] justify-center">
+                     <Map/>
+                    </DialogContent>
+                  </Dialog>
+                  <button type="submit" onClick={handleSubmit}>
+                    Submit
+                  </button>
                 </div>
               </DialogContent>
             </Dialog>
@@ -100,6 +170,51 @@ const Home = () => {
             <div className="w-28 h-28 bg-emerald-100 rounded-3xl" />
             <div className="w-72 h-28 bg-emerald-100 rounded-3xl" />
           </div>
+          <Dialog>
+            <DialogTrigger>
+              <button className="w-40 h-10 bg-emerald-100 rounded-3xl items-center flex justify-center mt-2">
+                <div className="text-green-800 text-xl font-bold font-['Fredoka']">
+                  Add Crop
+                </div>
+              </button>
+            </DialogTrigger>
+            <DialogContent className="bg-[#5caf74] justify-center">
+              <div className="mt-4 flex justify-center">
+                Add the Details for the Crop !
+              </div>
+              <div className="grid grid-col">
+                <div>
+                  <label>Enter the Planting date of the crop: </label>
+                  <input
+                    // onChange={handleInputChange}
+                    type="date"
+                    placeholder="Name of the farm"
+                    className="p-4 pl-10 pr-10 rounded-md"
+                  />
+                </div>
+
+                <div>
+                  <label>Select Crop: </label>
+                  <select
+                    className="p-4 pl-10 pr-10 rounded-md"
+                    // onChange={handleInputChange}
+                  >
+                    <option value="type1">Wheat</option>
+                    <option value="type2">SugarCane</option>
+                    <option value="type3">Rice</option>
+
+                    {/* Add more options as needed */}
+                  </select>
+                </div>
+                <button
+                  type="submit"
+                  //  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
--- frontend/src/Pages/Map.jsx
+++ frontend/src/Pages/Map.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const Map = () => {
+  // State variable to store the marker coordinates
+  const [markerCoordinates, setMarkerCoordinates] = useState([33.6844, 73.0479]);
+
   useEffect(() => {
-    const nustCoordinates = [33.6844, 73.0479]; // NUST Islamabad coordinates
-
-    const mymap = L.map("map").setView(nustCoordinates, 13);
-
+    const mymap = L.map("map").setView(markerCoordinates, 13);
+    
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
     }).addTo(mymap);
 
     // Set the height of the map container to 100vh and the width to 100vw
-    document.getElementById("map").style.height = "100vh";
-    document.getElementById("map").style.width = "100vw";
+    document.getElementById("map").style.height = "95vh";
+    document.getElementById("map").style.width = "70vw";
 
     // Create a close button
     const closeButton = document.createElement("div");
@@ -27,21 +28,29 @@ const Map = () => {
     closeButton.style.border = "1px solid #999";
     closeButton.style.padding = "5px";
     closeButton.style.borderRadius = "5px";
-    closeButton.style.zIndex = 4; // Set the z-index
-
-    closeButton.onclick = () => {
-      // Handle routing when the close button is clicked
-      // For this example, it navigates to the path "/"
-      window.location.href = "/";
-    };
+    closeButton.style.zIndex = 1; // Set the z-index
 
     // Append the close button to the parent container
     document.getElementById("map").appendChild(closeButton);
 
+    // Create a draggable marker
+    const marker = L.marker(markerCoordinates, { draggable: true }).addTo(mymap);
+
+    // Event listener for dragend
+    marker.on("dragend", (event) => {
+      const newCoordinates = event.target.getLatLng();
+      setMarkerCoordinates([newCoordinates.lat, newCoordinates.lng]);
+      localStorage.setItem('lat', newCoordinates.lat);
+      localStorage.setItem('lng', newCoordinates.lng);
+      console.log("Marker dragged to:", markerCoordinates);
+    });
+
     return () => {
       mymap.remove();
     };
-  }, []);
+  }, [markerCoordinates]); // Include markerCoordinates as a dependency for the useEffect
+
+  // Use markerCoordinates as needed in your component or pass it to the backend
 
   return (
     <>
